d8f50224bc91a64b76795d775c1b34c5a5587781
FengCastelo <ianfcastelo7@gmail.com>
2025-10-08 01:21:42 -0300

    Creating Database issues and first entity

--- src/main/java/fengcastelo/spring/developStock/Model/Entities/Product.java
+++ src/main/java/fengcastelo/spring/developStock/Model/Entities/Product.java
@@ -1,10 +1,24 @@
 package fengcastelo.spring.developStock.Model.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Entity
+@Table(name= "table_product")
 public class Product {
 
-    private Long id;
+    @Id
+    private UUID id;
     private String name;
     private Double price;
+    private Integer quantity;
 
 
 }
