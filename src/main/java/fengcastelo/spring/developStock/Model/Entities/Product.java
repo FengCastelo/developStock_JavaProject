90483aaf8c52cd7fce0d722bde366f3aa552d66d
FengCastelo <ianfcastelo7@gmail.com>
2025-10-08 00:44:23 -0300

    Project Created

---
+++ src/main/java/fengcastelo/spring/developStock/Model/Entities/Product.java
@@ -0,0 +1,10 @@
+package fengcastelo.spring.developStock.Model.Entities;
+
+public class Product {
+
+    private Long id;
+    private String name;
+    private Double price;
+
+
+}
---
+++ src/test/java/fengcastelo/spring/developStock/DevelopStockApplicationTests.java
@@ -0,0 +1,13 @@
+package fengcastelo.spring.developStock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
